82828041558a357daa01b183ba8c68f4fe863d23
MohamedBenAhmed55 <mohamed.benahmed@etudiant-fsegt.utm.tn>
2024-12-28 22:09:59 +0100

    Exception Filter

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +8,8 @@ async function bootstrap() {
   //   bufferLogs: true,
   // });
   // app.useLogger(app.get(MyLoggerService))
+  const { httpAdapter} = app.get(HttpAdapterHost)
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter))
   app.enableCors()
   app.setGlobalPrefix('api')
   await app.listen(process.env.PORT ?? 3000);
